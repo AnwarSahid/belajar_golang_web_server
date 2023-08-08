6bb992892f8dacf38e4505220ae78ac3b64e5882
AnwarSahid <anwarsahid28oktober@gmail.com>
2023-08-08 14:30:21 +0700

    addperson

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 var PORT = ":8000"
 
 type Person struct {
 	ID      int
-	Nama    string
+	Name    string
 	Age     int
 	Address string
 }
@@ -24,6 +25,7 @@ var person = []Person{
 func main() {
 	http.HandleFunc("/", greet)
 	http.HandleFunc("/person", getperson)
+	http.HandleFunc("/addperson", addperson)
 	http.ListenAndServe(PORT, nil)
 }
 
@@ -40,4 +42,27 @@ func getperson(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.Error(w, "invalid Method", http.StatusBadRequest)
+}
+
+func addperson(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		age := r.FormValue("age")
+		address := r.FormValue("address")
+		converage, err := strconv.Atoi(age)
+
+		if err != nil {
+			http.Error(w, "Invalid Age", http.StatusBadRequest)
+			return
+		}
+
+		newPerson := Person{len(person) + 1, name, converage, address}
+		person = append(person, newPerson)
+
+		json.NewEncoder(w).Encode(newPerson)
+		return
+	}
+	http.Error(w, "invalid method", http.StatusBadRequest)
 }
